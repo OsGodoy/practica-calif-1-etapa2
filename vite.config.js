6e043ac31f00e1a8b250f59216041f799e23c23c
Pedro Godoy <pedrosvaldogodoy@gmail.com>
2025-09-24 16:57:23 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/practica-calif-1-etapa2",
 });
